48e464310f782f18a2bae8085d81c7b5578b5120
trandung <dungtran.dev31@gmail.com>
2026-05-04 18:13:33 +0700

    chore: configure infrastructure services via docker compose and update ignore patterns

--- src/main/java/org/example/agapibeassignment/AgapiBeAssignmentApplication.java
+++ src/main/java/org/example/agapibeassignment/AgapiBeAssignmentApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AgapiBeAssignmentApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AgapiBeAssignmentApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AgapiBeAssignmentApplication.class, args);
+    }
 
 }
