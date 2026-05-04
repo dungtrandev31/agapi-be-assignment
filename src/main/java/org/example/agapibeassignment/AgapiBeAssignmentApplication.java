f0765c06ff99d2e1bce7106c3cf8bb4e352beff4
trandung <dungtran.dev31@gmail.com>
2026-05-04 18:03:44 +0700

    feat: initialize  project with infra

---
+++ src/main/java/org/example/agapibeassignment/AgapiBeAssignmentApplication.java
@@ -0,0 +1,13 @@
+package org.example.agapibeassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgapiBeAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgapiBeAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/agapibeassignment/AgapiBeAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.agapibeassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgapiBeAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
